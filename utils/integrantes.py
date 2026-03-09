5f493935f6449f4b3dac4b1141c29cf43be50f43
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2026-03-08 22:19:33 -0300

    Att Integrantes

--- utils/integrantes.py
+++ utils/integrantes.py
@@ -1,7 +1,9 @@
+# Integrantes - RMs
+
 dict_integrantes = {
     "Guilherme Gama": "565293",
     "Bruno Fernandes Nascimento": "552574",
     "Edgar Lódula de Assis": "565260",
     "Júlia Aben-Athar": "566325",
-    "Igor": ""
+    "Igor Thiago Nakajima Vieira": "563632"
 }
